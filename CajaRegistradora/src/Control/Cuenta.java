5936b56b77ed015e14278dc912e48cc6bf908e27
yilber822 <yilber822@gmail.com>
2020-03-06 15:44:21 -0500

    Prueba pasada

--- CajaRegistradora/src/Control/Cuenta.java
+++ CajaRegistradora/src/Control/Cuenta.java
@@ -15,10 +15,5 @@ public class Cuenta {
     public String documento;
     public int mesa;
     public String pago;
-    public innt prueba1;
-    public innt prueba4;
-    public innt prueba3;
-    public innt prueba2;
-    
-    
+ 
 }
