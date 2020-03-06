fe0bbe5f1b76909378df4cb36d2d52e65c586f2b
yilber822 <yilber822@gmail.com>
2020-03-06 15:08:36 -0500

    Modificaciones de prueba

--- CajaRegistradora/src/Control/Cuenta.java
+++ CajaRegistradora/src/Control/Cuenta.java
@@ -15,7 +15,10 @@ public class Cuenta {
     public String documento;
     public int mesa;
     public String pago;
-    
+    public innt prueba1;
+    public innt prueba4;
+    public innt prueba3;
+    public innt prueba2;
     
     
 }
